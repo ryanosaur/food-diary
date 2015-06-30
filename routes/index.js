55ad8e109430a68f6a5959a41ccd1f445bdd5718
Ryano <ryan.justin.taylor@gmail.com>
2015-06-30 12:31:57 -0700

    jangular + nexpress quotes app

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data.json');
+var fs = require('fs');
+
+router.get('/', function(req, res) {
+  res.render('index', {});
+});
 
 router.get('/data/:id', function(req, res) {
   var data = require("../data.json");
   res.send(data.quotes[+req.params.id - 1]);
 });
 
-router.get('/data', function(req, res) {
-  res.send("Got it!\n");
+router.get('/quotes', function(req, res) {
+  res.json(data);
 });
 
-router.post('/data', function(req, res) {
-  res.send("Posted!\n");
+router.post('/quotes', function(req, res) {
+  var body = req.body;
+  console.log(body);
+  data.quotes.push(body.quote);
+
+  fs.writeFile('./data.json', JSON.stringify(data), function(err){
+    if(err){
+      console.log(err);
+      respone.status(500).send('GTFO, this didn\'t work. FIle system error or somehting.');
+    }
+    res.json(data);
+  });
 });
 
 module.exports = router;
