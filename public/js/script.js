655a7daa3064a0b080b6a113969d28eb6ecfaaae
Ryano <ryan.justin.taylor@gmail.com>
2015-06-30 17:26:20 -0700

    updated to add CRD, next up - update

--- public/js/script.js
+++ public/js/script.js
@@ -15,10 +15,21 @@ angular.module('quotes', [])
   $scope.postQuote = function(){
     $http.post('http://localhost:3000/quotes', {quote: $scope.quote})
     .success(function(data){
-      $scope.quotes = data.quotes;
+      $scope.quotes.push(data.quote);
     })
     .error(function(error){
-      console.log(error)
+      console.log(error);
+    });
+    $scope.quote = '';
+  }
+
+  $scope.deleteQuote = function(index){
+    $http.delete('http://localhost:3000/quotes/' + index)
+    .success(function(data){
+      $scope.quotes.splice(index, 1);
+    })
+    .error(function(error){
+      console.log(error);
     });
     $scope.quote = '';
   }
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,35 @@ var router = express.Router();
 var data = require('../data.json');
 var fs = require('fs');
 
+var persistData = function(callbacks){
+  fs.writeFile('./data.json', JSON.stringify(data), function(err){
+    if(err){
+      callbacks.error();
+    }
+    callbacks.success();
+  });
+}
+
 router.get('/', function(req, res) {
   res.render('index', {});
 });
 
-router.get('/data/:id', function(req, res) {
-  var data = require("../data.json");
-  res.send(data.quotes[+req.params.id - 1]);
+router.delete('/quotes/:id', function(req, res) {
+  var id = req.params.id;
+  if(data.quotes[id]){
+    data.quotes.splice(id, 1);
+    persistData({
+      success: function(){
+        res.json({message: 'quoted deleted'});
+      },
+      error: function(){
+        respone.status(400).json({message: 'quote was not deleted!'});
+      }
+    });
+  }
+  else{
+    res.status(404).json({message: 'quote not found.'});
+  }
 });
 
 router.get('/quotes', function(req, res) {
@@ -17,16 +39,25 @@ router.get('/quotes', function(req, res) {
 });
 
 router.post('/quotes', function(req, res) {
-  var body = req.body;
-  console.log(body);
-  data.quotes.push(body.quote);
+  var quote = req.body.quote;
+  if(!quote){
+    res.status(400).json({error: 'yo, put in a quote.'});
+    return;
+  }
 
-  fs.writeFile('./data.json', JSON.stringify(data), function(err){
-    if(err){
-      console.log(err);
+  if(data.quotes.indexOf(quote) > -1){
+    res.status(400).json({error: 'dupe data'});
+    return;
+  }
+
+  data.quotes.push(quote);
+  persistData({
+    success: function(){
+      res.json({quote: quote});
+    },
+    error: function(){
       respone.status(500).send('GTFO, this didn\'t work. FIle system error or somehting.');
     }
-    res.json(data);
   });
 });
 
